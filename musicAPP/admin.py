4f0459718862ef83a85287ef6c7369e3afda34dd
RubenBelmonte <rbl_94@hotmail.com>
2016-04-05 12:53:08 +0200

    db_creation

--- musicAPP/admin.py
+++ musicAPP/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+import models
 
 # Register your models here.
+admin.site.register(models.Artist)
+admin.site.register(models.Album)
+admin.site.register(models.Track)
+
+admin.site.register(models.ArtistReview)
+admin.site.register(models.AlbumReview)
+admin.site.register(models.TrackReview)
