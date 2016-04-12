7106fdb8ff6d063201c8974f95dee29ad97f22d6
alexgg94 <alexgg94@hotmail.com>
2016-04-12 18:05:02 +0200

    views and urls

---
+++ musicAPP/urls.py
@@ -0,0 +1,48 @@
+from django.conf.urls import patterns, url
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import UpdateView
+from django.views.generic.base import RedirectView
+
+import models
+import forms
+import views
+
+urlpatterns = patterns('',
+    # Home page
+    url(r'^$',
+        RedirectView.as_view(url=reverse_lazy('artist_list', kwargs={'extension': 'html'})),
+        name='home_page'),
+
+    # List artists: /musicAPP/artists.json
+    url(r'^artists\.(?P<extension>(json|xml|html))$',
+        ArtistList.as_view(),
+        name='artist_list'),
+
+    # Artist details, ex.: /musicAPP/artists/1.json
+    url(r'^artists/(?P<pk>\d+)\.(?P<extension>(json|xml|html))$',
+        ArtistDetail.as_view(),
+        name='artist_detail'),
+
+
+    # List albums: /musicAPP/albums.json
+    url(r'^albums\.(?P<extension>(json|xml|html))$',
+        AlbumList.as_view(),
+        name='album_list'),
+
+    # Albums details, ex.: /musicAPP/albums/1.json
+    url(r'^albums/(?P<pk>\d+)\.(?P<extension>(json|xml|html))$',
+        AlbumDetail.as_view(),
+        name='album_detail'),
+
+
+    # List tracks: /musicAPP/tracks.json
+    url(r'^tracks\.(?P<extension>(json|xml|html))$',
+        TrackList.as_view(),
+        name='track_list'),
+
+    # Tracks details, ex.: /musicAPP/tracks/1.json
+    url(r'^tracks/(?P<pk>\d+)\.(?P<extension>(json|xml|html))$',
+        TrackDetail.as_view(),
+        name='track_detail'),
+
+)
--- musicAPP/views.py
+++ musicAPP/views.py
@@ -1,3 +1,92 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.utils import timezone
+from django.core import serializers
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, ListView
+from django.views.generic.base import TemplateResponseMixin
+from django.views.generic.edit import CreateView
+
+import models
+import forms
+
+class ConnegResponseMixin(TemplateResponseMixin):
+
+    def render_json_object_response(self, objects, **kwargs):
+        json_data = serializers.serialize(u"json", objects, **kwargs)
+        return HttpResponse(json_data, content_type=u"application/json")
+
+    def render_xml_object_response(self, objects, **kwargs):
+        xml_data = serializers.serialize(u"xml", objects, **kwargs)
+        return HttpResponse(xml_data, content_type=u"application/xml")
+
+    def render_to_response(self, context, **kwargs):
+        if 'extension' in self.kwargs:
+            try:
+                objects = [self.object]
+            except AttributeError:
+                objects = self.object_list
+            if self.kwargs['extension'] == 'json':
+                return self.render_json_object_response(objects=objects)
+            elif self.kwargs['extension'] == 'xml':
+                return self.render_xml_object_response(objects=objects)
+        return super(ConnegResponseMixin, self).render_to_response(context)
+
+class ArtistList(ListView, ConnegResponseMixin):
+    model = Artist
+
+    def get_queryset(self):
+        return Artist.objects.filter(artist=self.kwargs['pk'])
+
+class ArtistDetail(DetailView, ConnegResponseMixin):
+    model = Artist
+    template_name = 'artist_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ArtistDetail, self).get_context_data(**kwargs)
+        context['RATING_CHOICES'] = ArtistReview.RATING_CHOICES
+        return context
+
+class AlbumList(ListView, ConnegResponseMixin):
+    model = Album
+
+    def get_queryset(self):
+        return Album.objects.filter(album=self.kwargs['pk'])
+
+class AlbumDetail(DetailView, ConnegResponseMixin):
+    model = Album
+    template_name = 'album_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AlbumDetail, self).get_context_data(**kwargs)
+        context['RATING_CHOICES'] = AlbumReview.RATING_CHOICES
+        return context
+
+class TrackList(ListView, ConnegResponseMixin):
+    model = Track
+
+    def get_queryset(self):
+        return Track.objects.filter(track=self.kwargs['pk'])
+
+class TrackDetail(DetailView, ConnegResponseMixin):
+    model = Track
+    template_name = 'track_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TrackDetail, self).get_context_data(**kwargs)
+        context['RATING_CHOICES'] = TrackReview.RATING_CHOICES
+        return context
+
+"""
+def review(request, pk):
+    restaurant = get_object_or_404(Restaurant, pk=pk)
+    review = RestaurantReview(
+        rating=request.POST['rating'],
+        comment=request.POST['comment'],
+        user=request.user,
+        restaurant=restaurant)
+    review.save()
+    return HttpResponseRedirect(reverse('myrestaurants:restaurant_detail', args=(restaurant.id,)))
+"""
